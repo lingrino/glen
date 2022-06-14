8bca484ccd08522fab762d85daa7cc30528cb9dd
Sean Lingren <sean@lingrino.com>
2022-06-14 10:16:00 -0700

    remove spaces and add contributor

--- cmd/root.go
+++ cmd/root.go
@@ -20,19 +20,14 @@ const (
 func glenCmd() *cobra.Command {
 	// recurse determines if glen with also get variables from the project's parent groups
 	var recurse bool
-
 	// apiKey is the GitLab key that we should use when calling the API
 	var apiKey string
-
 	// directory is the path to the git repo that we should run glen on
 	var directory string
-
 	// remoteName is the name of the GitLab remote in your git repo
 	var remoteName string
-
 	// outputFormat is the text format that we should use to print our results to stdout
 	var outputFormat string
-
 	// groupOnly determines if glen only gets variables from the project's parent groups
 	var groupOnly bool
 
