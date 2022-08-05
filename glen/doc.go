09a8408007fc7c2f1c69437251ba1ac26016e94f
Sean Lingren <sean@lingrino.com>
2022-08-04 18:00:08 -0700

    gofmt

--- glen/doc.go
+++ glen/doc.go
@@ -4,13 +4,13 @@ Package glen provides an API to get GitLab project and group variables
 Glen has a VERY simple API with two structs that combined can get GitLab environment variables from
 a local repo with a GitLab remote.
 
-Repo
+# Repo
 
 The Repo struct holds information about a local repo and can be passed to a Variables struct. By
 default Repo assumes that your current working directory has a remote named 'origin'. However, you
 can specify custom directtories and custom origin names.
 
-Variables
+# Variables
 
 The Variables struct holds information about a GitLab project's CI/CD variables. By default
 Variables collects a project's variables using your GITLAB_TOKEN environment variable API key.
